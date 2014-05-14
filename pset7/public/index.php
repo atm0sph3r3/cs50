4f6a23d36f1d9c1c5d95962b7cf15ccd1d715931
Tim Balasavage <tim@timbalasavage.com>
2014-05-13 20:44:52 -0400

    Remove old copy of pset7

--- pset7/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // configuration
-    require("../includes/config.php"); 
-   
-    // Dispaly the user portfolio using a User object
-    $user = User::getUser();
-    render("portfolio.php", ["title" => "Portfolio","user"=>$user,"results"=>$user->portfolio()]);
-?>
